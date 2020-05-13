6aa1835e9a51d45ca8900d836c8e281d8ef85bda
shidqi <nauvalholic@gmail.com>
2020-05-13 11:18:13 +0700

    fix ajax datatables

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const getAllData = async () => {
-    const allData = await fetch("https://coronavirus-19-api.herokuapp.com/all");
+    const allData = await fetch('https://coronavirus-19-api.herokuapp.com/all');
     return allData.json();
   };
 
   const getCountries = async () => {
-    const allData = await fetch("https://coronavirus-19-api.herokuapp.com/countries");
+    const allData = await fetch('https://coronavirus-19-api.herokuapp.com/countries');
     return allData.json();
   };
 
   (async () => {
     const data = await getAllData();
     console.log(data);
-    $("#cases").text(data.cases.toLocaleString());
-    $("#deaths").text(data.deaths.toLocaleString());
-    $("#recovered").text(data.recovered.toLocaleString());
+    $('#cases').text(data.cases.toLocaleString());
+    $('#deaths').text(data.deaths.toLocaleString());
+    $('#recovered').text(data.recovered.toLocaleString());
   })();
 
   (async () => {
     const data = await getCountries();
-
-    let no = 0;
-    const dataHandler = data
-      .map(country => {
-        no += 1;
-        return `<tr><td> ${no}</td><td> ${country.country}</td>
-      <td style="background:#ffeaa7;"> ${country.cases}</td>
-      <td> ${country.todayCases}</td>
-      <td style="background:#fab1a0;"> ${country.deaths}</td>
-      <td> ${country.todayDeaths}</td>
-      <td style="background:#81ecec;"> ${country.recovered}</td>
-      <td> ${country.active}</td>
-      <td> ${country.critical}</td></tr>`;
-      })
-      .join("");
-    $("#countries").html(dataHandler);
-    $("#datatables").DataTable({ responsive: true });
+    console.log(data);
+    let no = 1;
+    $('#datatables').DataTable({
+      data: data,
+      columns: [
+        {
+          data: null,
+          render: function (data, type, row, meta) {
+            return meta.row + meta.settings._iDisplayStart + 1;
+          },
+        },
+        { data: 'country' },
+        { data: 'cases' },
+        { data: 'deaths' },
+        { data: 'todayCases' },
+        { data: 'todayDeaths' },
+        { data: 'recovered' },
+        { data: 'active' },
+        { data: 'critical' },
+      ],
+      responsive: true,
+    });
   })();
 });
